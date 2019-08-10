2a3cf54fde59a1e7b3cdc01a2aaca989f9245077
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2019-08-10 21:10:41 +0800

    Add Vuetify and Vue Router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import Router from 'vue-router'
 import App from './App.vue'
+import 'vuetify/dist/vuetify.min.css'
+import routes from '@/routes/routes'
 
 Vue.config.productionTip = false
 
+//vuetify
+Vue.use(Vuetify)
+
+//vue-router
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  base: __dirname,
+  routes
+})
+
 new Vue({
+  Vuetify,
+  router,
   render: h => h(App),
 }).$mount('#app')
