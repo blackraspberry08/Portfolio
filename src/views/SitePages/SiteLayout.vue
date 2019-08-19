f1f819f27262a0e581e370b696359ea203b98a62
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2019-08-19 23:16:50 +0800

    REmove Mojs

--- src/views/SitePages/SiteLayout.vue
+++ src/views/SitePages/SiteLayout.vue
@@ -15,8 +15,6 @@
 </template>
 
 <script>
-import mojs from '@mojs/core'
-
 export default {
   name: 'site-layout',
   props: {
@@ -26,50 +24,5 @@ export default {
       default: undefined
     }
   },
-  data: () => ({
-    burstShape: undefined,
-    bubbleShape: undefined,
-  }),
-  mounted(){
-    this.burstShape = new mojs.Burst({
-      radius: { 0: 30 },
-      angle: 'rand(0, 360)',
-      timeline: {delay: 0},
-      children: {
-        shape: 'line',
-        stroke: 'white',
-        fill: 'none',
-        scale: 1,
-        scaleX: { 1: 0 },
-        easing: 'cubic.out',
-        duration: 2000
-      }
-    }),
-    this.bubbleShape = new mojs.Burst({
-      radius: 28,
-      count: 3,
-      timeline: { delay: 100 },
-      children: {
-        stroke: 'white',
-        fill: 'none',
-        scale: 1,
-        strokeWidth: { 8: 0 },
-        radius: { 0 : 'rand(6, 10)' },
-        degreeShift: 'rand(-50, 50)',
-        duration: 800,
-        delay: 'rand(0, 250)',
-      }
-    })
-    let top = Math.floor(Math.random() * 100) + '%'
-    let left = Math.floor(Math.random() * 100) + '%'
-    let position = {top, left}
-    this.burstShape.tune(position).generate().replay()
-    this.bubbleShape.tune(position).generate().replay()
-  }
 }
 </script>
-<style>
-.dev-b{
-  border: 1px solid red;
-}
-</style>
